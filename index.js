1cb444c55b43a5790593fe03180361386189c53e
kimda-bin <rlaekqls0504@gmail.com>
2025-02-06 23:36:27 +0900

    MongoDB+Postman

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 데이터를 분석하여 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 데이터 분석하여 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 
@@ -17,6 +26,30 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다
+  const user = new User(req.body);
+
+  // user.save((err, userInfo) => {
+  //   if (err) return res.json({ success: false, err });
+  //   return res.status(200).json({
+  //     success: true,
+  //   });
+  // });
+
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
